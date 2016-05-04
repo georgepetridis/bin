e55f76e55b0215a0a72a33ed72e92a213f9a1545
George Petridis <george@georgepetridis.com>
2016-05-03 23:35:13 -0400

    code improvement

--- create_files.sh
+++ create_files.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 if [ -f $HOME/.bash_profile ]
 	then
-		echo ".bash_profile already exists. Cannot create another one."
+		while true; do
+			read -p ".bash_profile already exists. Do you want to delete it and create a new one? (y/n)" yn
+			case $yn in
+				[Yy]* ) rm $HOME/.bash_profile; break;;
+				[Nn]* ) exit;;
+				* ) echo "Enter y or n.";;
+			esac
+		if [ -f $HOME/bin/example.bash_profile ]; then
+			cp $HOME/bin/example.bash_profile $HOME/.bash_profile
+		fi
+		done
 	else
 		if [ -f $HOME/bin/example.bash_profile ]; then
 			cp $HOME/bin/example.bash_profile $HOME/.bash_profile
